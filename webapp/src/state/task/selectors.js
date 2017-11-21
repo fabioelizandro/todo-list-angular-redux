e765ff86c7b30ec04a496705a23509101bcd4b4f
Fabio Elizandro <fabio@elizandro.com.br>
2017-11-22 00:22:40 +1100

    Change selector to prevent unecessary re-renders.

--- webapp/src/state/task/selectors.js
+++ webapp/src/state/task/selectors.js
@@ -1,7 +1,1 @@
-export const selectTasks = state => {
-  return state.task.tasks.map(task => ({
-    id: task.id,
-    description: task.description,
-    done: task.done
-  }));
-};
+export const selectTasks = state => state.task.tasks;
